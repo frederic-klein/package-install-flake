919f45157ec0812936b76b15bea34ff913d2e2f4
Frederic K. <frederic-klein@users.noreply.github.com>
2026-02-09 18:40:51 +0100

    trying to update installed flake

--- nixpkgs/bootstrap/flake.nix
+++ nixpkgs/bootstrap/flake.nix
@@ -1,10 +1,11 @@
 {
-  description = "Ubuntu system bootstraping scripts";
+  description = "bootstraping scripts for Ubunut based systems";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
   outputs = { self, nixpkgs }:
     let
+      version = "1.0.0";
       systems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = f:
         nixpkgs.lib.genAttrs systems (system:
@@ -14,6 +15,8 @@
           }
         );
     in {
+      inherit version;
+      
       packages = forAllSystems ({ pkgs, system }: {
         system-bootstrap = pkgs.callPackage ./default.nix {};
         default = self.packages.${system}.system-bootstrap;
